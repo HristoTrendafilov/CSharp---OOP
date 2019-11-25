ff5b2b97ef15fba9414fb6a949ce22f18ff2db7d
HristoTrendafilov <57861356+HristoTrendafilov@users.noreply.github.com>
2019-11-25 17:32:25 +0200

    Add files via upload

---
+++ Telephony/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace Telephony
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+       {
+            var phoneNumbers = Console.ReadLine().Split().ToList();
+            var sites = Console.ReadLine().Split().ToList();
+
+            var smartphone = new Smartphone(phoneNumbers,sites);
+            Console.WriteLine(smartphone.GetNumbers());
+            Console.WriteLine(smartphone.GetSites());
+       }
+    }
+}
---
+++ Telephony/Smartphone.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Telephony
+{
+    class Smartphone : ICalling, IBrowsing
+    {
+        private List<string> phoneNumbers;
+        private List<string> sites;
+
+        public Smartphone(List<string> phoneNumbers, List<string> sites)
+        {
+            this.phoneNumbers = phoneNumbers;
+            this.sites = sites;
+        }
+
+        public string GetNumbers()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < phoneNumbers.Count; i++)
+            {
+                if (phoneNumbers[i].Any(char.IsLetter))
+                {
+                    sb.AppendLine("Invalid number!");
+                }
+                else
+                {
+                    sb.AppendLine($"Calling... {phoneNumbers[i]}");
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string GetSites()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < sites.Count; i++)
+            {
+                if (sites[i].Any(char.IsDigit))
+                {
+                   sb.AppendLine("Invalid URL!");
+                }
+                else
+                {
+                    sb.AppendLine($"Browsing: {sites[i]}!");
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
