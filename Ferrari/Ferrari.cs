438c138b6167253f8d17db6e36e74e6080f7cd6d
HristoTrendafilov <57861356+HristoTrendafilov@users.noreply.github.com>
2019-11-25 17:40:53 +0200

    Add files via upload

---
+++ Ferrari/Ferrari.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferrari
+{
+    public class Ferrari : IFerrari
+    {
+        private const string ferrariName = "488-Spider";
+
+        public Ferrari(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public string UseBrakes()
+        {
+            return "Brakes!";
+        }
+
+        public string UseGas()
+        {
+            return "Gas!";
+        }
+        public override string ToString()
+        {
+            return $"{ferrariName}/{UseBrakes()}/{UseGas()}/{this.Name}";
+        }
+    }
+}
---
+++ Ferrari/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ferrari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var ferrari = new Ferrari(name);
+            Console.WriteLine(ferrari);
+        }
+    }
+}
