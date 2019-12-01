ce45732851e5734a48fb8439aa8861ff71a5e49a
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2019-12-01 15:50:39 +0200

    Add files via upload

---
+++ CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,40 @@
+﻿using CommandPattern.Core.Commands;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var commandArr = args
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            
+            var commandName = (commandArr[0] + "Command").ToLower();
+            var commandArgs = commandArr.Skip(1).ToArray();
+
+            var commandType = Assembly.GetCallingAssembly().GetTypes()
+                .FirstOrDefault(n => n.Name.ToLower() == commandName);
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+            
+            var instanceType = Activator.CreateInstance(commandType) as ICommand;
+
+            if (instanceType == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            var result = instanceType.Execute(commandArgs);
+            return result;
+        }
+    }
+}
---
+++ CommandPattern/Core/Engine.cs
@@ -0,0 +1,27 @@
+﻿using CommandPattern.Core;
+using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern
+{
+    internal class Engine : IEngine
+    {
+        private ICommandInterpreter command;
+
+        public Engine(ICommandInterpreter command)
+        {
+            this.command = command;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                var result = command.Read(input);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
